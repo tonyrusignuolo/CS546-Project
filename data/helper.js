4a2d00958a96813c1f69f23a1350443c1a423008
Tony <tony.rusignuolo@gmail.com>
2019-04-16 17:44:10 -0400

    added seeds and testing

--- data/helper.js
+++ data/helper.js
@@ -1,12 +1,14 @@
-async function convertId(id) {
-	if (typeof id === "string") {
-		try {
-			id = ObjectID.createFromHexString(id);
-		} catch (err) {
-			throw "index convertID id format"
+module.exports = {
+	async convertId(id) {
+		if (typeof id === "string") {
+			try {
+				id = ObjectID.createFromHexString(id);
+			} catch (err) {
+				throw "index convertID id format"
+			}
+		} else if (!id._bsontype && id._bsontype !== "ObjectID") {
+			throw "index convertID id type";
 		}
-	} else if (!id._bsontype && id._bsontype !== "ObjectID") {
-		throw "index convertID id type";
+		return id;
 	}
-	return id;
 }
