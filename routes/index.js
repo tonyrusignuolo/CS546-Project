1e3320186580943abc961455c93493501e9ca4c1
Tony <tony.rusignuolo@gmail.com>
2019-04-25 18:09:11 -0400

    Added design system

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,30 @@
 const profile = require('./profile');
 const appointments = require('./appointments');
 const admin = require('./admin');
+const Handlebars = require('handlebars');
+
 
 const constructorMethod = (app) => {
 	app.use("/profile", profile);
 	app.use("/appointments", appointments);
 	app.use("/admin", admin);
-	app.use("*", (req, res) => {
-		res.sendStatus(404);
+	app.get("*", (req, res) => {
+		// res.sendStatus(404);
+		try {
+			// user = {
+			// 	role: 2,
+			// 	firstName: "Anthony"
+			// }
+			options = {
+				title: "Testing!",
+				pageType: "dashboard-page",
+				// user: user
+			}
+			res.render("partials/pages/landing.hbs", options);
+		} catch (error) {
+			res.status(400);
+			res.send(error);
+		}
 	})
 };
 
