28924c85e7ee35944d277f7670a57a2c6b56c8db
mmacari7 <mmacari7@gmail.com>
2019-04-30 19:29:01 -0400

    Figured out functionality for async maps + ajax calls w jQuery

--- public/map/js/healthmap.js
+++ public/map/js/healthmap.js
@@ -1,10 +1,65 @@
 // Simple JS for manipulating the maps page
 var map;
-function initMap() {
+async function initMap() {
     console.log("Jello Man")
+    //console.log(tacoSalad)
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.743991, lng: -74.032363},
         zoom: 12
     });
-    var marker = new google.maps.Marker({position: {lat: 40.743991, lng: -74.032363}, map: map});
 }
+
+function addMarker(coords){
+    var marker = new google.maps.Marker({position: coords, map: map})
+}
+
+
+// var tacoSalad;
+(async function($){
+    // var locObj = JSON.parse($("#t1").val())
+    // console.log(locObj)
+    // setTimeout(function(){
+        
+    // }, 2000);
+    await initMap()
+    //var marker = new google.maps.Marker({position: {lat: 40.743991, lng: -74.032363}, map: map});
+    
+
+    var requestConfig = {
+        method: "GET",
+        url: "/map/all",
+        dataType: "json",
+    }
+    
+    $.ajax(requestConfig).then(function(response){
+        console.log(response)
+        let ll = 40.743991;
+        let ln = -74.032363
+        for(let i=0; i < 20; i++){
+            addMarker({lat: ll, lng: ln})
+            ll += 0.001
+            ln += 0.001
+        }
+        // addMarker({lat: 40.743991, lng: -74.032363})
+        //var marker = new google.maps.Marker({position: {lat: 40.743991, lng: -74.032363}, map: map})
+    })
+
+    // $.ajax(requestConfig).then(function(response){
+    //     console.log(response)
+    // })
+    // var requestConfig = {
+    //     method: "GET",
+    //     url: "/map/all",
+    //     dataType: "json",
+    // }
+
+    // $.ajax(requestConfig).then(function(response){
+    //     console.log(response)
+    // })
+
+    // //console.log("Hello World")
+    // tacoSalad = "Set in the jQuery"
+
+//    var marker = new google.maps.Marker({position: {lat: 40.743991, lng: -74.032363}, map: map});
+
+})(jQuery)
--- routes/map.js
+++ routes/map.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const {ObjectID} = require("mongodb");
+const practitioners = require('../data/practitioners');
 
 router.get('/', async (req, res) => {
     res.render('map/map.hbs', {layout: false});
 });
+
+router.get('/all', async(req, res) => {
+    let allPractitioners;
+    try {
+        allPractitioners = await practitioners.getAll();
+    } catch (e) {
+        res.status(500);
+        res.send(e);
+        return;
+    }
+    res.send(allPractitioners)
+})
 
 module.exports = router;
